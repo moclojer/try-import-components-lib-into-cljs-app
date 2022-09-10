50a444ed0ec4dfc84f7e10e943056a8859ccc7bd
Rafael Delboni <rafadelboni@gmail.com>
2022-09-10 14:10:21 -0300

    feat: Changes to show how iterop with react

---
+++ lib/src/components/Banner.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export class Banner extends React.Component {
+  render(){
+    return <div> Hello From React.Component</div>
+  }
+}
--- src/cljs/app/core.cljs
+++ src/cljs/app/core.cljs
@@ -1,14 +1,33 @@
 (ns app.core
-  (:require [helix.core :refer [defnc $]]
-            [helix.hooks :as hooks]
+  (:require ["moclojer-components" :as mc]
+            ["rc-input-number$default" :as InputNumber]
+            ["react-dom/client" :as rdom]
+            [helix.core :refer [$ defnc]]
             [helix.dom :as d]
-            ["react"  :as React]
-            ["moclojer-components" :as mc]
-            ["react-dom/client" :as rdom]))
+            [helix.hooks :as hooks]))
+
+
+(defnc my-component
+  [{:keys [on?]}]
+  (d/div
+   {:style {:background (if on? "red" "black")}}
+   "Hello, Helix!"))
 
 (defnc app []
   (let [[state set-state] (hooks/use-state {:name "Helix User"})]
-    (d/div
+   (d/div
+
+     ;; Example using an helix wrapped component
+     ($ my-component {:on? true})
+
+     ;; Example using an react component on local repo
+     ($ mc/Banner {})
+
+     ;; Example using an react component npm lib
+     (d/div ($ InputNumber {:upHandler (d/div {:style {:color "blue"}} "^")
+                            :downHandler (d/div {:style {:color "red"}} "V")
+                            :defaultValue 10}))
+
      (d/input {:value (:name state)
                :on-change #(set-state assoc :name (.. % -target -value))}))))
 
